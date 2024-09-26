76e9d34321cee7fc145fe3b96c2ec4516c037053
kreu2e <kreuzework@gmail.com>
2024-09-26 17:19:52 +0300

    Schedule added

--- AlabugaBots/obj/Debug/net8.0/AlabugaBots.AssemblyInfo.cs
+++ AlabugaBots/obj/Debug/net8.0/AlabugaBots.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlabugaBots")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63cb3fe965a928c9ce5e57dea1bbcc0cc379839f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11166ef63950c3771027dbd7a7fb3a859e9de2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlabugaBots")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlabugaBots")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
